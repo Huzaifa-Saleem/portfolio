7d8186d0fff03728f99b3e4056e575a89caede95
Huzaifa Saleem <89271698+Huzaifa-Saleem@users.noreply.github.com>
2024-05-02 02:06:17 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,9 +49,7 @@ export default function RootLayout({
             <ThemeSwitch />
           </ActiveSectionContextProvider>
         </ThemeContextProvider>
-      </body>
-      <!--Start of Tawk.to Script-->
-<script type="text/javascript">
+        <script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -62,7 +60,7 @@ s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
 </script>
-<!--End of Tawk.to Script-->
+      </body> 
     </html>
   );
 }
