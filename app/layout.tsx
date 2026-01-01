af316d65daf4709d2981706e86e0f9db8d24d781
Huzaifa Saleem <89271698+Huzaifa-Saleem@users.noreply.github.com>
2026-01-01 03:10:51 -0800

    Comment out layout elements for future use
    Comment out Header, Footer, and background divs in layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 `}
       >
-        <div
+        {/* <div
           style={{
             animationDuration: "15s",
           }}
@@ -37,14 +37,14 @@ export default function RootLayout({
             animationDuration: "15s",
           }}
           className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394] animate-spin"
-        ></div>
+        ></div> */}
 
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
-            <Header />
+            {/* <Header /> */}
               <Analytics />
               {children}
-            <Footer />
+            {/* <Footer /> */}
 
             <Toaster position="top-right" />
             <ThemeSwitch />
